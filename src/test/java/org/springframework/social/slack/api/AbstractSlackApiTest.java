23420a9ea51e41e9a7b298c8689c0527dd7e796a
Jeff Tsay <jctsay@aim.com>
2016-08-27 20:30:55 -0700

    Get APP_ACCESS_TOKEN for testing from a property file
    which should be in

--- src/test/java/org/springframework/social/slack/api/AbstractSlackApiTest.java
+++ src/test/java/org/springframework/social/slack/api/AbstractSlackApiTest.java
@@ -1,15 +1,31 @@
-
 package org.springframework.social.slack.api;
 
-import org.junit.Before;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.social.slack.api.impl.SlackTemplate;
 import org.springframework.social.test.client.MockRestServiceServer;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.junit.Before;
+
 public class AbstractSlackApiTest {
 			
-	protected static final String APP_ACCESS_TOKEN = "xoxp-3105667121-3107455865-XXXXXXXXXX-XXXXXXX";
+	protected static final String APP_ACCESS_TOKEN;
+
+	static {
+		final Properties properties = new Properties();
+		try (final InputStream stream =
+				 AbstractSlackApiTest.class.getResourceAsStream("test.properties")) {
+			properties.load(stream);
+
+			APP_ACCESS_TOKEN = properties.getProperty("appAccessToken");
+		} catch (IOException ioe) {
+			throw new RuntimeException("Can't load test.properties from classpath", ioe);
+		}
+	}
 
 	protected SlackTemplate slackTemplate;
 	protected SlackTemplate slack;
